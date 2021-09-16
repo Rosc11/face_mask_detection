fceede748fb0268d614cc03f4e444e2712eedb68
Robin Schrijvers <robin.schrijvers@pxl.be>
2021-09-16 11:17:57 +0200

    nano changes

--- parameters.py
+++ parameters.py
@@ -2,7 +2,7 @@
 
 SEND_INTERVAL = 0 #in milliseconds
 
-CONFIG_FILE_PATH = "/home/ghip-xavier/face_mask_detection/mask_detector_config.txt"
+CONFIG_FILE_PATH = "/home/ghip-nano/face_mask_detection/mask_detector_config.txt"
 
 ENDPOINT = 'http://localhost:8095/api/current_frame'
 
--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-cd /home/ghip-xavier/face_mask_detection/
-/usr/bin/python3 /home/ghip-xavier/face_mask_detection/mask_detector_cam.py no-stream
+cd /home/ghip-nano/face_mask_detection/
+/usr/bin/python3 /home/ghip-nano/face_mask_detection/mask_detector_cam.py no-stream
